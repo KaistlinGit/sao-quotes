451036d90e97f170968fb0395f42ba0b5fea01bb
KaistlinGit <kaistlinmail@gmail.com>
2013-05-20 00:01:34 +0200

    Ajout css

--- application/views/templates/default.php
+++ application/views/templates/default.php
@@ -1,3 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" 
+   "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
+<head>
+	<title>TCG Manager</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<?php 
+		// CSS
+		echo HTML::style('css/default.css');
+
+		/*
+		// JS
+		echo HTML::script('js/lib/jquery.min.js'); 
+		echo HTML::script('js/script.js'); 
+
+		// DATATABLE
+		echo HTML::script('js/lib/dataTables/js/jquery.dataTables.min.js'); 
+		echo HTML::style('js/lib/dataTables/css/jquery.dataTables.css');*/
+	?>
+</head>
 <html>
     <head></head>
     <body>
